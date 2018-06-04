3244a19196dd2b5b07303091c442c8cffa69f7b9
Simon Cain <rudenik@gmail.com>
2018-06-04 15:59:41 -0400

    basic layout started

---
+++ assets/javascript/app.js
@@ -0,0 +1,9 @@
+// import Typed  from './typed.js';
+
+var options = {
+    strings: ["Are you classified as human?", "Negative, I am a Meat popsicle"],
+    typeSpeed: 40,
+    loop: true,
+
+}
+var typed = new Typed(".typed", options);
