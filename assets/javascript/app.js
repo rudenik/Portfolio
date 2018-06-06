0d44301511dd70e9a670633dfdb3fa44398046e2
Simon Cain <rudenik@gmail.com>
2018-06-06 12:47:27 -0400

    got the basicScroll working.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,3 +7,40 @@ var options = {
 
 }
 var typed = new Typed(".typed", options);
+
+const instance = basicScroll.create({
+	elem: document.querySelector('#justme'),
+	from: 'top-bottom',
+    to: 'bottom-top',
+	props: { "--translateX": {
+        from: '200px',
+        to: '100px',
+        timing: 'linear'
+        }
+    }
+}).start();
+
+const nextIns = basicScroll.create({
+    elem: document.querySelector("#aboutimage"),
+    from: 'top-bottom',
+    to: 'bottom-top',
+    props: { "--translateX": {
+        from: '100px',
+        to: '0',
+        timing: 'linear'
+    }
+
+    }
+}).start()
+// instance.start();
+
+//this ain't working either. Wanna go back to trying to basicScroll on this.
+// $(window).scroll(function(){
+//     console.log($(this).scrollTop());
+//     if($(this).scrollTop()>=200){
+//         $(".hero-image").addClass("animate");
+//     }else{
+//         $(".hero-image").removeClass("animate");
+//     }
+    
+// })
