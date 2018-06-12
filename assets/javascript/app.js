b5679139561976db81c9a9c0c28768f7c43db56d
Simon Cain <rudenik@gmail.com>
2018-06-12 12:22:55 -0400

    I think it's done.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,3 @@
-
-/********************************************************
-THIS IS THE APP FILE
- ********************************************************/
 var options = {
     strings: ["Are you classified as human?", "Negative, I am a Meat popsicle"],
     typeSpeed: 40,
@@ -10,18 +6,37 @@ var options = {
 }
 var typed = new Typed(".typed", options);
 
-// document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-//     anchor.addEventListener('click', function (e) {
-//         e.preventDefault();
+window.onload = function() {
+    if (window.innerWidth <= 430){
+        $(".social-links").empty();
+        var iconArray = ["fab fa-linkedin", "fab fa-twitter", "fab fa-facebook-f", "fab fa-instagram", "far fa-envelope"];
+        var linkArray = ["https://www.linkedin.com/in/simon-cain-9159341a/", "https://twitter.com/rudenik", "https://www.facebook.com/", "https://www.instagram.com/rudenik/", "mailto:simon.cain@mail.utoronto.ca?subject=What's up with Simon Cain"];
+        var orderList = $("<ul>");
+        var socialName = ["LinkedIn", "Twitter", "Facebook", "Instagram", "Email"];
+        for (ele in iconArray){
+            var listItem = $("<li>");
+            var aTag = $('<a>')
+            aTag.attr("href", linkArray[ele]);
+            var iTag = $("<i>")
+            iTag.addClass(iconArray[ele]);
+            aTag.text(socialName[ele]);
+            aTag.prepend(iTag);
+            
+            aTag.appendTo(listItem);
+            listItem.appendTo(orderList);
+        }
+        orderList.appendTo($(".social-links"));
+    
+    };
+}
 
-//         document.querySelector(this.getAttribute('href')).scrollIntoView({
-//             behavior: 'smooth'
-//         });
-//     });
-// });
 $('a[href*=\\#]').on('click', function(event){     
     event.preventDefault();
+    if(window.innerWidth <= 990 ){
+        $('html,body').animate({scrollTop:$(this.hash).offset().top-280}, 500);
+    }else{
     $('html,body').animate({scrollTop:$(this.hash).offset().top-50}, 500);
+    }
 });
 
 const nextIns = basicScroll.create({
@@ -53,15 +68,3 @@ document.querySelectorAll('.scene').forEach((elem) => {
         }
     }).start();
 })
-// instance.start();
-
-//this ain't working either. Wanna go back to trying to basicScroll on this.
-// $(window).scroll(function(){
-//     console.log($(this).scrollTop());
-//     if($(this).scrollTop()>=200){
-//         $(".hero-image").addClass("animate");
-//     }else{
-//         $(".hero-image").removeClass("animate");
-//     }
-    
-// })
